ddfd708958a6c7ecacc86c03b2d6f47ee9cb1988
Maxpicca-Li <liyanqin@cqu.edu.cn>
2021-10-09 12:16:40 +0800

    pc参数取段设置，提高程序复用性

--- mycpu/branch_predict_global.v
+++ mycpu/branch_predict_global.v
@@ -32,29 +32,39 @@ module branch_predict_global #(parameter PHT_DEPTH = 6) // 作为端口参数
     
     integer i,j;
     wire predF,pred_takeD,pred_takeE,pred_takeM;
-    // wire [(PHT_DEPTH-1):0] PHT_indexF,PHT_indexD,PHT_indexE,PHT_indexM;
+    wire [(PHT_DEPTH-1):0] PHT_indexF,PHT_indexD,PHT_indexE,PHT_indexM;
 
 // ---------------------------------------预测逻辑---------------------------------------
+<<<<<<< HEAD
 // 取指阶段
     assign PHT_index = pcF[(PHT_DEPTH-1+2):2] ^ GHR_Spec[(PHT_DEPTH-1):0];
+=======
+    // 取指阶段
+    assign PHT_indexF = pcF[(PHT_DEPTH-1):0] ^ GHR_Spec[(PHT_DEPTH-1):0];
+>>>>>>> parent of d137031... update_GPHT_index涓ょ瑙ｅ喅鏂规
     assign predF = PHT[PHT_index][1];      // 在取指阶段预测是否会跳转，并经过流水线传递给译码阶段。
     assign pred_takeF = branchF & predF;
 
-// pipeline
+    // pipeline
     flopenrc #(1)         DFF_pred_takeD(clk,rst,flushD,~stallD,pred_takeF,pred_takeD);
-    // flopenrc #(PHT_DEPTH) DFF_PHT_indexD(clk,rst,flushD,~stallD,PHT_indexF,PHT_indexD);
+    flopenrc #(PHT_DEPTH) DFF_PHT_indexD(clk,rst,flushD,~stallD,PHT_indexF,PHT_indexD);
+    
     
     flopenrc #(1)         DFF_pred_takeE(clk,rst,flushE,ena,pred_takeD,pred_takeE);
-    // flopenrc #(PHT_DEPTH) DFF_PHT_indexE(clk,rst,flushE,ena,PHT_indexD,PHT_indexE);
+    flopenrc #(PHT_DEPTH) DFF_PHT_indexE(clk,rst,flushE,ena,PHT_indexD,PHT_indexE);
     
     flopenrc #(1)         DFF_pred_takeM(clk,rst,flushM,ena,pred_takeE,pred_takeM);
-    // flopenrc #(PHT_DEPTH) DFF_PHT_indexM(clk,rst,flushM,ena,PHT_indexE,PHT_indexM);
+    flopenrc #(PHT_DEPTH) DFF_PHT_indexM(clk,rst,flushM,ena,PHT_indexE,PHT_indexM);
 
+<<<<<<< HEAD
     // assign PHT_index = PHT_indexF;
     // assign update_PHT_index = PHT_indexM;
     assign update_PHT_index = pcM[(PHT_DEPTH-1+2):2] ^ GHR_Retire[(PHT_DEPTH-1):0];
     
 // GHR_Spec初始化及更新
+=======
+    // GHR_Spec初始化及更新
+>>>>>>> parent of d137031... update_GPHT_index涓ょ瑙ｅ喅鏂规
     always@(posedge clk) begin
         if(rst) begin
             // 初始化
@@ -105,5 +115,7 @@ module branch_predict_global #(parameter PHT_DEPTH = 6) // 作为端口参数
 // ---------------------------------------PHT初始化以及更新---------------------------------------
 
     // 译码阶段输出最终的预测结果
+    assign PHT_index = PHT_indexF;
+    assign update_PHT_index = PHT_indexM;
     assign correct = (actual_takeM  == pred_takeM)?1:0;
 endmodule
